abfddea347b33afe65f1c45ed05d0963c01c25a2
julietcohen <jscohen@bren.ucsb.edu>
2022-04-10 11:31:34 -0700

    syntax fix

--- Almond_model.R
+++ Almond_model.R
@@ -37,10 +37,10 @@ aanomaly = function(data, daily = FALSE, crop) {
   else if (crop == "almonds") {
     for (i in seq.int(from = min(unique(grouped_data$year)), to = max(unique(grouped_data$year)), by = 1)) {
     
-    yield_anomaly[i] <- ((-0.015 * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year == i]) - 
-      (0.0046 * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year == i] * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year == i]) -
-      (0.07 * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == i]) +
-      (0.0043 * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == i] * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == i]) +
+    yield_anomaly[i] <- ((-0.015 * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year[i]]) - 
+      (0.0046 * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year == [i]] * grouped_data$mean_tmin[grouped_data$month == 2 & grouped_data$year == [i]]) -
+      (0.07 * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == [i]]) +
+      (0.0043 * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == [i]] * grouped_data$mean_precip[grouped_data$month == 1 & grouped_data$year == [i]]) +
       0.28
       )
     print(paste0("yield anomaly for ", i, " is ", yield_anomaly[i]))
