c19ab410d341bf0228292beea17445d655406f8b
julietcohen <jscohen@bren.ucsb.edu>
2022-04-14 23:21:14 -0700

    made script and rmd for profit model

--- profit.R
+++ profit.R
@@ -4,52 +4,36 @@
 #'  This model computes the price anomaly for a yield of almonds
 #'  
 #' @param almond_anom dataframe output from the almond_yield() function with column headers `year` and `almond_anomaly` (tons/acre)
-#' @param price cost of each unit of almond_anomaly (US dollars/ton/acre) (default = 2.00)
+#' @param acres number of acres on almond farm 
+#' @param price cost of each unit of almond yield anomaly (US dollars/ton of almonds) (default = 2.00)
 #' @param discount discount rate (default = 0.12)
 #' 
-#' @return profit estimate (US dollars)
+#' @return profit in US dollars for almond yield anomalies summed across all years 
+#' 
 #' @authors Sydney Rilum, Juliet Cohen, Shale Hunter
 #'
 #' @examples
 #' 
 
 
-profit <- function(almond_anom, price = 2.00, default = 0.12) {
+profit <- function(almond_anom, acres, price = 2.00, discount = 0.12) {
  
   # pull data from columns in dataframe output from almond_anomaly function, or from dataframe the user inputs with the same columns
   year = (seq_along(almond_anom$year))
   yield = (almond_anom$almond_anomaly)
   
-  # calculate price as a vector 
-  #value = ()
-  value = price*yield
+  # calculate value as a vector 
+  value = price*yield*acres
   
   # net present value: account for discount rate with inflation
   profit = value / (1 + discount)**year
   
   # calculate total profit for all years
-  total_profit <- sum(profit)
+  #total_profit <- sum(profit)
   
-  return(total_profit)
+  return(profit)
 }
 
-
-# test function outside
-
-#almond_anom <- almond_anomaly(data = user_data)
-price = 2.00
-discount = 0.12
-year = (seq_along(almond_anom$year))
-year
-yield = (almond_anom$almond_anomaly)
-
-# calculate price as a vector 
-#value = ()
-value = price*yield
-value
-# net present value: account for discount rate with inflation
-profit = value / (1 + discount)**year # might need to reverse years, ask other groups
-sum(profit)
 
 
 
