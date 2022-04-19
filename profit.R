dd0d1298e31241af5ae84537a0391252ac4cabee
julietcohen <jscohen@bren.ucsb.edu>
2022-04-18 18:33:30 -0700

    finalized final docs

--- profit.R
+++ profit.R
@@ -1,7 +1,7 @@
 
 #' Profit Model for Almond Yield Anomaly
 #'  
-#'  This model computes the price anomaly for almond yields
+#' This model computes the profit anomaly for almond yield anomalies for each year in the range of years given and the total profit anomaly summed over all years
 #'  
 #' @param almond_anom dataframe output from the almond_yield() function with column headers `year` and `almond_anomaly` (units = tons/acre)
 #' @param acres number of acres on almond farm 
@@ -15,24 +15,27 @@
 #' @examples
 #' 
 
-# define the function arguments, setting default values for `price` and `discout` 
+# define the function arguments, setting default values for `price` and `discount` 
 profit <- function(df, acres, price = 2.00, discount = 0.12) {
- 
+  
   # pull data from the columns in the dataframe output from almond_anomaly function, called 'almond_anom', or from a user's almond anomaly dataframe with the same column names
   year = (seq_along(df$year))
+  time = (year - year[1])
   yield = (df$almond_anomaly)
   
   # calculate value as a vector 
   value = price*yield*acres
   
   # net present value: account for discount rate with inflation
-  profit = value / (1 + discount)**year
+  df$profit = value / (1 + discount)**time
+  # profit is a list of 22 values, representing the profit for each year
   
   # calculate total profit for all years
-  tot_profit <- sum(profit)
+  #tot_profit <- sum(profit)
   
- # return(profit)
-  return(tot_profit)
+  #return(profit)
+  #return(tot_profit)
+  return(list(year_profit_df = df[, c("year","profit")], sum = sum(df$profit)))
 }
 
 
